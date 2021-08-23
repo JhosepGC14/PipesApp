b537f19b7ffb10cd32aa1ac5fd090cc70d5e8abf
Jhosep Guadalupe <jhosep.guadalupe>
2021-08-23 10:15:39 -0500

    se realizo el datePipe y lower, title y upeerCase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,26 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRouterModule } from './app-router.module';
 
 import { AppComponent } from './app.component';
 
 import { SharedModule } from './shared/shared.module';
+import { VentasModule } from './ventas/ventas.module';
+
+//Cambiar el local de la app
+import localEs from '@angular/common/locales/es-PE';
+import { registerLocaleData } from '@angular/common';
+registerLocaleData(localEs);
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule, 
-    SharedModule, 
-    AppRouterModule
+  imports: [BrowserModule, SharedModule, AppRouterModule, VentasModule],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'es-PE',
+    },
   ],
-  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
